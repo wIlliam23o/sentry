c3b0e05d98f210324ddea9f1669700b3a487b419
Dan Fuller <dfuller@sentry.io>
2019-02-26 13:25:55 -0800

    refs(api): Default to using SnubaSearchBackend for search. Remove unused code from DjangoSearchBackend (SEN-181)
    Production is now in a world where we're entirely using Snuba for search. It's time to start relying
    on this in dev as well, so that we're getting the same results. Matt is working on
    https://github.com/getsentry/sentry/pull/12173, which will make it easy to run Snuba locally. So
    once that has been merged, we can merge this as well.
    
    Also removes unused `_query` code from `DjangoSearchBackend`, and cascades out to remove anything
    that is now unused due to that removal. I plan to just merge the django and snuba backends together,
    but this pr is already pretty big so holding off for now.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1063,7 +1063,7 @@ SENTRY_TAGSTORE_OPTIONS = (
 )
 
 # Search backend
-SENTRY_SEARCH = os.environ.get('SENTRY_SEARCH', 'sentry.search.django.DjangoSearchBackend')
+SENTRY_SEARCH = os.environ.get('SENTRY_SEARCH', 'sentry.search.snuba.SnubaSearchBackend')
 SENTRY_SEARCH_OPTIONS = {}
 # SENTRY_SEARCH_OPTIONS = {
 #     'urls': ['http://localhost:9200/'],
--- src/sentry/tagstore/base.py
+++ src/sentry/tagstore/base.py
@@ -57,7 +57,6 @@ class TagStorage(Service):
 
         'get_group_ids_for_users',
         'get_group_tag_values_for_users',
-        'get_group_ids_for_search_filter',
 
         'get_group_tag_keys_and_top_values',
 
@@ -67,7 +66,6 @@ class TagStorage(Service):
         'get_group_tag_value_iter',
 
         'get_group_tag_value_qs',
-        'get_event_tag_qs',
     ])
 
     __write_methods__ = frozenset([
@@ -347,12 +345,6 @@ class TagStorage(Service):
         """
         raise NotImplementedError
 
-    def get_event_tag_qs(self, project_id, environment_id, key, value):
-        """
-        >>> get_event_tag_qs(1, 2, 'environment', 'prod')
-        """
-        raise NotImplementedError
-
     def get_groups_user_counts(self, project_ids, group_ids, environment_ids):
         """
         >>> get_groups_user_counts([1, 2], [2, 3], [4, 5])
@@ -399,13 +391,6 @@ class TagStorage(Service):
     def get_group_tag_values_for_users(self, event_users, limit=100):
         """
         >>> get_group_tag_values_for_users([EventUser(1), EventUser(2)])
-        """
-        raise NotImplementedError
-
-    def get_group_ids_for_search_filter(
-            self, project_id, environment_id, tags, candidates=None, limit=1000):
-        """
-        >>> get_group_ids_for_search_filter(1, 2, [('key1', 'value1'), ('key2', 'value2')])
         """
         raise NotImplementedError
 
