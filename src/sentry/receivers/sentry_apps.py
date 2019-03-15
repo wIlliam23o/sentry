07083fc3dc5f543a8e0020baa5281fafa30089e3
denamwangi <dena@getsentry.com>
2019-03-15 13:39:06 -0700

    further consolidate issue_resolved signal

--- src/sentry/receivers/sentry_apps.py
+++ src/sentry/receivers/sentry_apps.py
@@ -3,12 +3,11 @@ from __future__ import absolute_import
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from sentry.models import Group, GroupAssignee, Organization
+from sentry.models import Group, GroupAssignee
 from sentry.signals import (
     issue_ignored,
     issue_assigned,
     issue_resolved,
-    resolved_with_commit,
 )
 from sentry.tasks.sentry_apps import (
     process_resource_change_bound,
@@ -56,19 +55,21 @@ def send_issue_assigned_webhook(project, group, user, **kwargs):
 
 
 @issue_resolved.connect(weak=False)
-def send_issue_resolved_webhook(project, group, user, resolution_type, **kwargs):
-    resolution_type = resolution_type
+def send_issue_resolved_webhook(organization_id, project, group, user, resolution_type, **kwargs):
+    data_resolution_type = resolution_type
     if resolution_type in ('in_next_release', 'in_release'):
-        resolution_type = 'resolved_in_release'
+        data_resolution_type = 'resolved_in_release'
+    elif resolution_type == 'in_commit':
+        data_resolution_type = 'resolved_in_commit'
     else:
-        resolution_type = 'resolved'
+        data_resolution_type = 'resolved'
 
     send_workflow_webhooks(
         project.organization,
         group,
         user,
         'issue.resolved',
-        data={'resolution_type': resolution_type},
+        data={'resolution_type': data_resolution_type},
     )
 
 
@@ -81,18 +82,6 @@ def send_issue_ignored_webhook(project, user, group_list, **kwargs):
             user,
             'issue.ignored',
         )
-
-
-@resolved_with_commit.connect(weak=False)
-def send_resolved_with_commit_webhook(organization_id, group, user, **kwargs):
-    organization = Organization.objects.get(id=organization_id)
-    send_workflow_webhooks(
-        organization,
-        group,
-        user,
-        'issue.resolved',
-        data={'resolution_type': 'resolved_in_commit'},
-    )
 
 
 def send_workflow_webhooks(organization, issue, user, event, data=None):
