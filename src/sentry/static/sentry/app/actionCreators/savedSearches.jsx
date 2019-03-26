24a64300ede7037343bb6b800d2fc86d42d3d75a
Billy Vong <billy@sentry.io>
2019-03-25 17:02:57 -0700

    fix msg

--- src/sentry/static/sentry/app/actionCreators/savedSearches.jsx
+++ src/sentry/static/sentry/app/actionCreators/savedSearches.jsx
@@ -84,7 +84,7 @@ export function pinSearch(api, orgId, query, type) {
     },
   });
 
-  promise.catch(handleXhrErrorResponse('Unable to un-pin search'));
+  promise.catch(handleXhrErrorResponse('Unable to pin search'));
 
   return promise;
 }
